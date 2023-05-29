8365b5488cf2fcab93296ebb4b8ea633d8027b01
Mohaned88 <mohanedabdallah88@gmail.com>
2023-05-29 16:24:22 +0300

    reg commit

--- lib/register.dart
+++ lib/register.dart
@@ -58,146 +58,200 @@ class _RegState extends State<Reg> {
     var width = MediaQuery.of(context).size.height;
     return Scaffold(
       body: Container(
-        width: width,
+        width: double.infinity,
         height: height,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/images/login_cover.png'),
+            fit: BoxFit.fill,
           ),
         ),
-        child: SingleChildScrollView(
-          child: ListView(
-            addAutomaticKeepAlives: true,
-            shrinkWrap: true,
-            children: [
-              SizedBox(
-                height: height * .05,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "مرحبا بكم في تطبيق دينامكس للمندوبين",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'GE SS Two',
+        child: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.all(width * 0.02),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  'تسجيل الدخول',
+                  textDirection: TextDirection.rtl,
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'GE SS Two',
+                    color: Colors.white,
+                  ),
+                ),
+                SizedBox(
+                  height: width*0.02,
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(width * 0.02),
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey,
+                          spreadRadius: 0.7,
+                          blurRadius: 1,
+                          offset: Offset(2, 1),
+                        ),
+                      ],
                     ),
-                  )
-                ],
-              ),
-              GestureDetector(
-                onTap: () {},
-                child: Container(
-                  width: MediaQuery.of(context).size.width * .25,
-                  height: MediaQuery.of(context).size.height * .4,
-                  child: Image.asset("assets/images/login.jpg"),
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.all(10),
-                child: TextFormField(
-                  key: _txtKey,
-                  controller: nameController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'اسم المندوب',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter Your Name ';
-                    }
-                    return null;
-                  },
-                ),
-              ),
-              Container(
-                padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
-                child: TextFormField(
-                  key: _txtpw,
-                  obscureText: true,
-                  controller: passwordController,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                    labelText: 'كلمة السر',
-                  ),
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Please enter password text';
-                    }
+                    child: SingleChildScrollView(
+                      child: Column(
+                        mainAxisSize: MainAxisSize.max,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image.asset('assets/images/login_icon.jpg',
+                            color: Color(0xff635AD9),
+                            width: width*0.5,
+                            height: width*0.5,
+                          ),
+                          Container(
+                            padding: EdgeInsets.all(10),
+                            child: TextFormField(
+                              key: _txtKey,
+                              controller: nameController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                labelText: 'اسم المندوب',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter Your Name ';
+                                }
+                                return null;
+                              },
+                            ),
+                          ),
+                          Container(
+                            padding: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                            child: TextFormField(
+                              key: _txtpw,
+                              obscureText: true,
+                              controller: passwordController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                labelText: 'كلمة السر',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter password text';
+                                }
 
-                    return null;
-                  },
-                ),
-              ),
-              Container(
-                width: width * .65,
-                padding: EdgeInsets.fromLTRB(20, 10, 20, 10),
-                child: ElevatedButton(
-                  onPressed: () async {
-                    if (nameController.text.isEmpty) {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                                title: const Text('تنبيه '),
-                                content: const Text('من فضلك ادخل كود المندوب'),
-                              ),);
-                    }
-                    else if (passwordController.text.isEmpty) {
-                      showDialog(
-                          context: context,
-                          builder: (BuildContext context) => AlertDialog(
-                                title: const Text('تنبيه '),
-                                content: const Text('من فضلك ادخل كلمة المرور'),
-                              ),);
-                    }
-                    else {
-                      getManData("${nameController.text}","${passwordController.text}")
-                          .then(
-                        (value) async {
-                          print(value.length);
-                          if (value.length > 100) {
-                            final prefs = await SharedPreferences.getInstance();
-                            await prefs.setDouble('tag', data![0]["Mandoubtag"]);
-                            await prefs.setDouble('safe_tag', data![0]["SafeAccId"]);
-                            await prefs.setInt('safe', data![0]["SafeId"]);
-                            await prefs.setInt('store', data![0]["StoreId"]);
-                            await prefs.setString('ID', '${data![0]["PersonId"]}');
-                            await prefs.setString('PersonName', '${data![0]["PersonName"]}');
-                            await prefs.setString('Password', '${data![0]["Password"]}');
-
-                            print("${data![0]["Mandoubtag"].toString()}");
-                            print("${data![0]["safe_tag"].toString()}");
-
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (_) => Login(
-                                  value: value,
-                                  personid: data![0]["PersonId"],
+                                return null;
+                              },
+                            ),
+                          ),
+                          Container(
+                            width: width,
+                            height: 80,
+                            padding: EdgeInsets.all(width * 0.02),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                backgroundColor: Color(0xff635AD9),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(width * 0.2),
                                 ),
                               ),
-                            );
-                          }
-                          else {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: Text("خطأ اسم المستخدم و كلمة المرور"),
-                              ),
-                            );
-                          };
-                        },
-                      );
-                      // Navigator.push(
-                      //     context, MaterialPageRoute(builder: (_) => Login()));
-                    }
-                  },
+                              onPressed: () async {
+                                if (nameController.text.isEmpty) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) => AlertDialog(
+                                      title: const Text('تنبيه '),
+                                      content:
+                                          const Text('من فضلك ادخل كود المندوب'),
+                                    ),
+                                  );
+                                } else if (passwordController.text.isEmpty) {
+                                  showDialog(
+                                    context: context,
+                                    builder: (BuildContext context) => AlertDialog(
+                                      title: const Text('تنبيه '),
+                                      content:
+                                          const Text('من فضلك ادخل كلمة المرور'),
+                                    ),
+                                  );
+                                } else {
+                                  getManData("${nameController.text}",
+                                          "${passwordController.text}")
+                                      .then(
+                                    (value) async {
+                                      print(value.length);
+                                      if (value.length > 100) {
+                                        final prefs =
+                                            await SharedPreferences.getInstance();
+                                        await prefs.setDouble(
+                                            'tag', data![0]["Mandoubtag"]);
+                                        await prefs.setDouble(
+                                            'safe_tag', data![0]["SafeAccId"]);
+                                        await prefs.setInt(
+                                            'safe', data![0]["SafeId"]);
+                                        await prefs.setInt(
+                                            'store', data![0]["StoreId"]);
+                                        await prefs.setString(
+                                            'ID', '${data![0]["PersonId"]}');
+                                        await prefs.setString('PersonName',
+                                            '${data![0]["PersonName"]}');
+                                        await prefs.setString(
+                                            'Password', '${data![0]["Password"]}');
 
-                  //forgot password screen
-                  child: Text('دخول'),
+                                        print(
+                                            "${data![0]["Mandoubtag"].toString()}");
+                                        print("${data![0]["safe_tag"].toString()}");
+
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (_) => Login(
+                                              value: value,
+                                              personid: data![0]["PersonId"],
+                                            ),
+                                          ),
+                                        );
+                                      } else {
+                                        ScaffoldMessenger.of(context).showSnackBar(
+                                          SnackBar(
+                                            content: Text(
+                                                "خطأ اسم المستخدم و كلمة المرور"),
+                                          ),
+                                        );
+                                      }
+                                      ;
+                                    },
+                                  );
+                                  // Navigator.push(
+                                  //     context, MaterialPageRoute(builder: (_) => Login()));
+                                }
+                              },
+                              child: Text(
+                                'دخول',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  fontFamily: 'GE SS Two',
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/splash/splash.dart
+++ lib/splash/splash.dart
@@ -18,39 +18,75 @@ class Splash extends StatelessWidget {
       () {
         Navigator.of(context).pushReplacement(
           MaterialPageRoute(
-            builder: (BuildContext context) => const Reg(),
-          ),
-        );
-      },
-    );
-    return Scaffold(
-      body: Center(
-        child: Container(
-          width: w*0.9,
-          height: w*0.9,
-          child: TranslationAnimatedWidget.tween(
-            enabled: true,
-            //update this boolean to forward/reverse the animation
-            translationDisabled: Offset(0, 320),
-            translationEnabled: Offset(0, 0),
-            child: OpacityAnimatedWidget.tween(
+            builder: (BuildContext context) => OpacityAnimatedWidget.tween(
               enabled: true,
               opacityDisabled: 0,
               opacityEnabled: 0.9,
               duration: Duration(milliseconds: 1600),
-              child: ScaleAnimatedWidget.tween(
+              child: const Reg(),
+            ),
+          ),
+        );
+      },
+
+    );
+    return Scaffold(
+      backgroundColor: Color(0xff635AD9),
+      body: Container(
+        width: w,
+        height: h,
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(w*0.2),
+            bottomRight: Radius.circular(w*0.2),
+          ),
+        ),
+        child: Stack(
+          alignment: Alignment.center,
+          fit: StackFit.expand,
+          children: [
+            Container(
+              width: w*0.9,
+              height: w*0.9,
+              child: TranslationAnimatedWidget.tween(
                 enabled: true,
+                //update this boolean to forward/reverse the animation
+                translationDisabled: Offset(0, 320),
+                translationEnabled: Offset(0, 0),
+                child: OpacityAnimatedWidget.tween(
+                  enabled: true,
+                  opacityDisabled: 0,
+                  opacityEnabled: 0.9,
+                  duration: Duration(milliseconds: 1600),
+                  child: ScaleAnimatedWidget.tween(
+                    enabled: true,
+                    duration: Duration(milliseconds: 1500),
+                    scaleDisabled: 0.5,
+                    scaleEnabled: 1,
+                    child: Image.asset(
+                      'assets/images/full_logo.png',
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
                 duration: Duration(milliseconds: 1500),
-                scaleDisabled: 0.5,
-                scaleEnabled: 1,
-                child: Image.asset(
-                  'assets/images/full_logo.png',
-                  fit: BoxFit.contain,
+              ),
+            ),
+            Positioned(
+              bottom: h * 0.05,
+              child: Text(
+                "مرحبا بكم في تطبيق دينامكس للمندوبين",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  fontFamily: 'GE SS Two',
+                  color: Colors.blueGrey,
                 ),
               ),
             ),
-            duration: Duration(milliseconds: 1500),
-          ),
+          ],
         ),
       ),
     );
